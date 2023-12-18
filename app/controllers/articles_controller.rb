fe51b69f47c9cbaadc821247f06d91dff241f2ec
jeremiahtenbrink@gmail.com <jeremiahtenbrink@gmail.com>
2023-12-18 12:52:05 -0700

    Add logic to save articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 class ArticlesController < ApplicationController
   def show
-    byebug
     @article = Article.find(params[:id])
   end
   def index
     @articles = Article.all
+  end
+  def new
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 end
